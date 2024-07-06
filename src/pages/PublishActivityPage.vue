c316c45a18a83d20a5c7cb8f20403b5c913c99fe
BrigiteT <21200164@ue.edu.pe>
2024-07-05 23:12:56 -0500

    Subiendo los cambios Login y Publicar Actividades

---
+++ src/pages/PublishActivityPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header>
+      <q-toolbar>
+        <q-toolbar-title> Publicar Actividades y Excursiones </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <q-page>
+        <publish-activity />
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import PublishActivity from "components/PublishActivity.vue";
+
+export default {
+  components: {
+    PublishActivity,
+  },
+};
+</script>
+
+<style scoped>
+/* Estilos específicos para esta página */
+</style>
